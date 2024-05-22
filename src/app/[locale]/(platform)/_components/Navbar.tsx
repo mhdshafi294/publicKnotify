45e7bb3afe758481e94779b1bab83944480c3fad
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2024-05-22 14:01:03 +0300

    deployement fix

--- src/app/[locale]/(platform)/_components/Navbar.tsx
+++ src/app/[locale]/(platform)/_components/Navbar.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 
 import DesktopNavbar from "./DesktopNavbar";
+import UserOptions from "./user-0ptions";
 
 const Navbar = () => {
   return (
     <div className="inset-0 absolute h-[71px] w-full bg-[#302939] flex justify-between items-center px-8">
       <p className="font-bold text-3xl text-white">Knotify</p>
       <DesktopNavbar />
+      <div className="flex md:hidden flex-row-reverse justify-end items-center h-full gap-6e">
+        <UserOptions />
+      </div>
     </div>
   );
 };
