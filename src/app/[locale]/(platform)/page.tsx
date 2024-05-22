51fda89bf2c84e33bbbdc3e19d0e19bbcb6389fb
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2024-05-22 13:25:26 +0300

    deployement fix

--- src/app/[locale]/(platform)/page.tsx
+++ src/app/[locale]/(platform)/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <p>{t("title")}</p>
+        {/* <p>{t("title")}</p> */}
       </main>
     </>
   );
