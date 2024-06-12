2701a56da1596279362fdf8ceb337871e8eef241
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2024-06-12 15:06:56 +0300

    modified the category section

--- src/app/[locale]/(platform)/_components/category-secrtion.tsx
+++ src/app/[locale]/(platform)/_components/category-secrtion.tsx
@@ -8,31 +8,37 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import MaxWidthContainer from "@/components/ui/MaxWidthContainer";
 
 const CategorySecrtion = async () => {
   const categories = await getCategory();
   const t = await getTranslations("Index");
 
   return (
-    <Carousel className="w-full">
-      <div className="flex justify-between items-center">
-        <h2 className="font-bold text-2xl capitalize">{t("moods")}</h2>
-        {/* <div className="flex relative justify-end items-center end-[80px]">
+    <MaxWidthContainer className="w-full px-0 ps-4">
+      <Carousel
+        opts={{ align: "start", containScroll: false, dragFree: true }}
+        className="w-full"
+      >
+        <div className="flex justify-between items-center">
+          <h2 className="font-bold text-2xl capitalize">{t("moods")}</h2>
+          {/* <div className="flex relative justify-end items-center end-[80px]">
           <CarouselPrevious />
           <CarouselNext />
         </div> */}
-      </div>
-      <CarouselContent className="w-full mt-5">
-        {categories.map((category) => (
-          <CarouselItem
-            key={category.id}
-            className="basis-1/2 md:basis-1/4 lg:basis-[14%]"
-          >
-            <CategoryCard key={category.id} category={category} />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-    </Carousel>
+        </div>
+        <CarouselContent className="w-full mt-5 -ml-4">
+          {categories.map((category) => (
+            <CarouselItem
+              key={category.id}
+              className="basis-auto md:basis-1/4 lg:basis-[14%]"
+            >
+              <CategoryCard key={category.id} category={category} />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </MaxWidthContainer>
   );
 };
 
