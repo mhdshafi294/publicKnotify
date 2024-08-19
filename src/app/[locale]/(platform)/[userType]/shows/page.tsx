cb52122a2e98eb1d23ab816ccb108954c75fc980
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2024-08-19 13:33:03 +0300

    solve conflict

--- src/app/[locale]/(platform)/[userType]/shows/page.tsx
+++ src/app/[locale]/(platform)/[userType]/shows/page.tsx
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
-import React from "react";
-
-const Page = () => {
-  return <div>Page</div>;
-};
-
-export default Page;
-=======
-
 const AllShowsPage = () => {
   return <div>AllShowsPage</div>;
 };
 
 export default AllShowsPage;
->>>>>>> 3f3f2057ce8d8f1fc52fb6120665cd94679da7f3
