624d51f36ce7483076bad9210d96ab649fd241ae
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2024-08-19 13:25:19 +0300

    podcaster dashboard

---
+++ src/app/[locale]/(platform)/[userType]/shows/[showId]/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PodcasterDashboard from "../_components/podcaster-dashboard";
+
+const Page = ({
+  params,
+  searchParams,
+}: {
+  params: { userType: string; showId: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) => {
+  return (
+    <main className="flex flex-1 flex-col items-start justify-start gap-6 w-full mb-2">
+      <PodcasterDashboard />
+    </main>
+  );
+};
+
+export default Page;
---
+++ src/app/[locale]/(platform)/[userType]/shows/_components/podcaster-dashboard.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import React from "react";
+
+const PodcasterDashboard = () => {
+  return (
+    <div className="flex-1 h-full flex w-full">
+      {/* Left Sidebar */}
+      <div className="bg-[#1a1929] w-80 "></div>
+      {/* Main Content */}
+      <div className="bg-background w-full flex-1 py-16 px-8 xl:px-16">
+        <div className="w-full grid grid-cols-3 gap-8">
+          {/* Content card */}
+          <div className="p-8 bg-[#1a1929] border border-card-foreground/10  h-[404px] rounded-[8px] shadow-[2px_2px_0px_0px_#302e3e]">
+            <div className="w-full h-[150px] relative grayscale">
+              <Image
+                src="/podcast-filler.webp"
+                alt="podcaster dashboard"
+                fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                className="object-cover rounded "
+              />
+              <h2>Publish an episode</h2>
+              <p>
+                In order to submit and publish your Show, you must first publish
+                an episode
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PodcasterDashboard;
