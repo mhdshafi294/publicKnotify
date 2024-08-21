0d34c0dcf354e5404bb12cad70d878a11bc9f930
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2024-08-21 10:18:13 +0300

    to pull

--- src/app/[locale]/(platform)/[userType]/publish/_components/form-header.tsx
+++ src/app/[locale]/(platform)/[userType]/publish/_components/form-header.tsx
@@ -7,6 +7,8 @@ import { SaveIcon } from "lucide-react";
 import ButtonLoader from "@/components/ui/button-loader";
 import { cn } from "@/lib/utils";
 import PublishButton from "./publish-button";
+import { useRouter } from "@/navigation";
+import { useSession } from "next-auth/react";
 
 type FormHeaderProps = {
   step: number;
@@ -63,54 +65,70 @@ const FormHeader: React.FC<FormHeaderProps> = ({
   isPublished,
   uploadedNewPodcast,
   t,
-}) => (
-  <div className="flex items-center justify-between w-full">
-    <div className="flex flex-col justify-between lg:justify-center items-start">
-      <h1 className="text-xl font-bold">{t("podcastDraftLabel")}</h1>
+}) => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  return (
+    <div className="flex items-center justify-between w-full">
+      <div className="flex flex-col justify-between lg:justify-center items-start">
+        <h1 className="text-xl font-bold">{t("podcastDraftLabel")}</h1>
+      </div>
+      <div className="flex items-center justify-end gap-2">
+        <Button
+          className="capitalize text-sm lg:hidden"
+          variant="secondary"
+          onClick={() => setIsShow(!isShow)}
+          type="button"
+        >
+          {t("draftsButton")}
+        </Button>
+        <Button
+          disabled={isPending}
+          className={cn("capitalize mt-0 text-sm", { hidden: step === 2 })}
+          variant="secondary"
+          type="button"
+          onClick={() => {
+            router.push(`/profile/${session?.user?.id}`);
+          }}
+        >
+          {isPending ? <ButtonLoader /> : t("cancel")}
+        </Button>
+        <Button
+          disabled={isPending}
+          className={cn("capitalize mt-0 text-sm", { hidden: step === 2 })}
+          variant="default"
+          type="submit"
+        >
+          {isPending ? <ButtonLoader /> : t("saveButton")}
+        </Button>
+        <Button
+          disabled={isPending}
+          className={cn("capitalize mt-0 text-sm", { hidden: step === 1 })}
+          variant="secondary"
+          type="button"
+          onClick={onBack}
+        >
+          {t("backButton")}
+        </Button>
+        <Button
+          disabled={isPending}
+          className={cn("capitalize mt-0 text-sm", { hidden: step === 1 })}
+          variant="outline"
+          type="submit"
+        >
+          <SaveIcon className="size-4 me-1.5" strokeWidth={1.5} />
+          {isPending ? <ButtonLoader /> : t("saveButton")}
+        </Button>
+        <PublishButton
+          className={cn({ hidden: step === 1 })}
+          podcast_id={podcast_id!}
+          disabled={isPublished || !podcast_id || !uploadedNewPodcast}
+          selectedPlatform={selectedPlatforms}
+        />
+      </div>
     </div>
-    <div className="flex items-center justify-end gap-2">
-      <Button
-        className="capitalize text-sm lg:hidden"
-        variant="secondary"
-        onClick={() => setIsShow(!isShow)}
-        type="button"
-      >
-        {t("draftsButton")}
-      </Button>
-      <Button
-        disabled={isPending}
-        className={cn("capitalize mt-0 text-sm", { hidden: step === 2 })}
-        variant="default"
-        type="submit"
-      >
-        {isPending ? <ButtonLoader /> : t("continueButton")}
-      </Button>
-      <Button
-        disabled={isPending}
-        className={cn("capitalize mt-0 text-sm", { hidden: step === 1 })}
-        variant="secondary"
-        type="button"
-        onClick={onBack}
-      >
-        {t("backButton")}
-      </Button>
-      <Button
-        disabled={isPending}
-        className={cn("capitalize mt-0 text-sm", { hidden: step === 1 })}
-        variant="outline"
-        type="submit"
-      >
-        <SaveIcon className="size-4 me-1.5" strokeWidth={1.5} />
-        {isPending ? <ButtonLoader /> : t("saveButton")}
-      </Button>
-      <PublishButton
-        className={cn({ hidden: step === 1 })}
-        podcast_id={podcast_id!}
-        disabled={isPublished || !podcast_id || !uploadedNewPodcast}
-        selectedPlatform={selectedPlatforms}
-      />
-    </div>
-  </div>
-);
+  );
+};
 
 export default FormHeader;
