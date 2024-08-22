beebb61bcdf537bf3ba12c0d48877c8c00ff0d22
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2024-08-22 10:10:45 +0300

    set up episodes page

--- src/app/[locale]/(platform)/[userType]/shows/[showId]/episodes/pages.tsx
+++ src/app/[locale]/(platform)/[userType]/shows/[showId]/episodes/pages.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import EpisodesPageContent from "./_components/episodes-page-content";
 
-const EpisodesPage = () => {
-  return <div>EpisodesPage</div>;
+const EpisodesPage = ({
+  params,
+  searchParams,
+}: {
+  params: { userType: string; showId: string; episodeId: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) => {
+  return <EpisodesPageContent {...{ params, searchParams }} />;
 };
 
 export default EpisodesPage;
