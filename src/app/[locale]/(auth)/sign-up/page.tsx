0a1e15b7691817f0133e7b55131da507c31c3f28
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2024-10-03 19:13:50 +0300

    updated auth form tabs UI

--- src/app/[locale]/(auth)/sign-up/page.tsx
+++ src/app/[locale]/(auth)/sign-up/page.tsx
@@ -29,37 +29,31 @@ const SignUp = ({
         className="md:w-[550px] flex flex-col items-center px-3 md:px-0"
       >
         {/* Tabs List: container for the tab triggers */}
-        <TabsList className="w-full h-14 grid grid-cols-3 bg-transparent dark:text-white rounded-md mb-6 px-0 border-none border-transparent">
+        <TabsList className="w-full h-14 grid grid-cols-3 gap-28 justify-items-center bg-transparent dark:text-white rounded-md mb-6 px-0 border-none border-transparent">
           {/* Tab Trigger for 'podcaster' */}
-          <TabsTrigger
-            value="podcaster"
-            className="font-normal md:px-0 md:place-self-center"
-          >
+          <TabsTrigger value="podcaster" className="font-normal md:px-0  ">
             {t("podcaster")}
           </TabsTrigger>
           {/* Tab Trigger for 'user' */}
-          <TabsTrigger
-            value="user"
-            className="font-normal md:px-0 md:place-self-start"
-          >
+          <TabsTrigger value="user" className="font-normal md:px-0 ">
             {t("user")}
           </TabsTrigger>
           {/* Tab Trigger for 'company' */}
-          <TabsTrigger
-            value="company"
-            className="font-normal md:px-0 md:place-self-end"
-          >
+          <TabsTrigger value="company" className="font-normal md:px-0 ">
             {t("company")}
           </TabsTrigger>
         </TabsList>
+
         {/* Tab Content for 'podcaster' */}
         <TabsContent value="podcaster">
           <SignUpForm type="podcaster" />
         </TabsContent>
+
         {/* Tab Content for 'user' */}
         <TabsContent value="user">
           <SignUpForm type="user" />
         </TabsContent>
+
         {/* Tab Content for 'company' */}
         <TabsContent value="company">
           <SignUpForm type="company" />
